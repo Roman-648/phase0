5bcb5e2410b21e0142edd159b9993183d5311e2f
Brandon864 <ojbrandon2345@mail.com>
2024-10-02 11:02:19 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+// Arrays - used to store a coolection items
+/// create an array 
+// Accessing items in an array/looping through an array
+// Arrya methods
+
+
+const students =  [ "Faith","Rose",34,43,4 true,[12,45,67],null]
+const ages = new Array( "Faith","Rose",34,43,4)
+
+console.log(students)
